ce4fba71852b74dba0faea7c0d8f66488f98649a
vaibhav Patil <vp14541610@gmail.com>
2025-12-06 12:44:58 +0530

    Update settings.py

--- schoolsite/schoolsite/settings.py
+++ schoolsite/schoolsite/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # add near top (after SecurityMiddleware)
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'schoolsite.urls'
 
